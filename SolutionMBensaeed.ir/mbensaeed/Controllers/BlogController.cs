fab3d94d4b39a33b53d6938d7e41b1cbf93d7fb8
mbensaeed <mbensaeed@bipc.org.ir>
2019-10-30 18:27:29 +0330

    Comlete Blog

--- SolutionMBensaeed.ir/mbensaeed/Controllers/BlogController.cs
+++ SolutionMBensaeed.ir/mbensaeed/Controllers/BlogController.cs
@@ -27,7 +27,7 @@ namespace mbensaeed.Controllers
             PagingStatus.PageIndex = (Page ?? 1) - 1;
             var ListAllPost = GetAllPost(PagingStatus.PageIndex, PagingStatus.ItemsPerPage, out int totalCount);
             var result = new StaticPagedList<vm_AllPost>(ListAllPost, PagingStatus.PageIndex + 1, PagingStatus.ItemsPerPage, totalCount);
-            return View();
+            return View(result);
         }
         public IEnumerable<vm_AllPost> GetAllPost(int page, int ItemsPerPage, out int totalCount)
         {
@@ -40,23 +40,25 @@ namespace mbensaeed.Controllers
                 var _activity = _objEntityActivity.GetAll();
 
                 var _objEntityImage = new RepositoryPattern<Image>(_Context);
-                var _image = _objEntityPost.GetAll();
+                var _image = _objEntityImage.GetAll();
 
                 var Result = new List<vm_AllPost>();
                 Result = (from pst in _post
-                          join ac in _activity
-                          on pst.ID equals ac.Posts.ID
+                          //join ac in _activity
+                          //on pst.ID equals ac.Posts.ID into act
+                          //from PstAc in act.DefaultIfEmpty()
                           select new vm_AllPost
                           {
-                              ImageID =pst.ImageID,
+                              PostID=pst.ID,
                               Content = pst.Content,
-                              LikeCount = 1,//totalCount(ac.Posts.ID),
+                              LikeCount =1,//totalCount(ac.Posts.ID),
+                              Category="ورزش",//pst.Category.DescriptionFa,
                               Labels = pst.Labels,
                               PostDate = pst.PostDate,
                               PostTime = pst.PostTime,
                               Title = pst.Title,
                               ViewCount = 2,
-                              ImageUrl =pst.Image.FileUrl,
+                              ImageUrl =_image.Single(x=>x.ID==pst.ImageID).FileUrl, //pst.Image.FileUrl,
                               IsActive = pst.IsActive
                           }
                             ).OrderByDescending(x => x.PostTime)
@@ -70,9 +72,47 @@ namespace mbensaeed.Controllers
                 return Result;
             }       
         }
+        [HttpPost]
         public ActionResult ContentDetail()
         {
             return View();
+        }
+        [HttpGet]
+        public ActionResult ContentDetail(int PostID)
+        {
+            using (var _Context = new ApplicationDbContext())
+            {
+                var _objEntityPost = new RepositoryPattern<Post>(_Context);
+                var _post = _objEntityPost.SearchFor(x => x.ID == PostID ).ToList();
+
+                //var _objEntityActivity = new RepositoryPattern<Activity>(_Context);
+                //var _activity = _objEntityActivity.GetAll();
+
+                var _objEntityImage = new RepositoryPattern<Image>(_Context);
+                var _image = _objEntityImage.GetAll();
+
+                var Result = new List<vm_AllPost>();
+                Result = (from pst in _post
+                              //join ac in _activity
+                              //on pst.ID equals ac.Posts.ID into act
+                              //from PstAc in act.DefaultIfEmpty()
+                          select new vm_AllPost
+                          {
+                              PostID = pst.ID,
+                              Content = pst.Content,
+                              LikeCount = 1,//totalCount(ac.Posts.ID),
+                              Category = "ورزش",//pst.Category.DescriptionFa,
+                              Labels = pst.Labels,
+                              PostDate = pst.PostDate,
+                              PostTime = pst.PostTime,
+                              Title = pst.Title,
+                              ViewCount = 2,
+                              ImageUrl = _image.Single(x => x.ID == pst.ImageID).FileUrl, //pst.Image.FileUrl,
+                              IsActive = pst.IsActive
+                          }
+                            ).ToList();
+
+                return View(Result);
         }
     }
 }
--- SolutionMBensaeed.ir/mbensaeed/ViewModels/vm_AllPost.cs
+++ SolutionMBensaeed.ir/mbensaeed/ViewModels/vm_AllPost.cs
@@ -6,8 +6,16 @@ using System.Web;
 
 namespace mbensaeed.ViewModels
 {
-    public class vm_AllPost : Post
+    public class vm_AllPost 
     {
+        public int PostID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string IsActive { get; set; }
+        public string PostDate { get; set; }
+        public string PostTime { get; set; }
+        public string Labels { get; set; }
+        public string Category { get; set; }
         public int ViewCount { get; set; }
         public int LikeCount { get; set; }
         public string ImageUrl { get; set; }
