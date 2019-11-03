8e2fdf806e4230fd76c414d9b1a54c8bc31c61ee
mbensaeed <mbensaeed@bipc.org.ir>
2019-11-03 18:25:44 +0330

    Weblog

--- SolutionMBensaeed.ir/mbensaeed/Controllers/BlogController.cs
+++ SolutionMBensaeed.ir/mbensaeed/Controllers/BlogController.cs
@@ -25,52 +25,18 @@ namespace mbensaeed.Controllers
         {
             ViewBag.Title = "عنوان آخرين پست";
             PagingStatus.PageIndex = (Page ?? 1) - 1;
-            var ListAllPost = GetAllPost(PagingStatus.PageIndex, PagingStatus.ItemsPerPage, out int totalCount);
+            var ListAllPost = GetPost(PagingStatus.PageIndex, PagingStatus.ItemsPerPage, out int totalCount);
             var result = new StaticPagedList<vm_AllPost>(ListAllPost, PagingStatus.PageIndex + 1, PagingStatus.ItemsPerPage, totalCount);
             return View(result);
         }
-        public IEnumerable<vm_AllPost> GetAllPost(int page, int ItemsPerPage, out int totalCount)
+        public IEnumerable<vm_AllPost> GetPost(int page, int ItemsPerPage, out int totalCount)
         {
-            using (var _Context = new ApplicationDbContext())
-            {
-                var _objEntityPost = new RepositoryPattern<Post>(_Context);
-                var _post = _objEntityPost.SearchFor(x => x.IsActive == "1").ToList();
-
-                var _objEntityActivity = new RepositoryPattern<Activity>(_Context);
-                var _activity = _objEntityActivity.GetAll();
-
-                var _objEntityImage = new RepositoryPattern<Image>(_Context);
-                var _image = _objEntityImage.GetAll();
-
-                var Result = new List<vm_AllPost>();
-                Result = (from pst in _post
-                          //join ac in _activity
-                          //on pst.ID equals ac.Posts.ID into act
-                          //from PstAc in act.DefaultIfEmpty()
-                          select new vm_AllPost
-                          {
-                              PostID=pst.ID,
-                              Content = pst.Content,
-                              LikeCount =1,//totalCount(ac.Posts.ID),
-                              Category="ورزش",//pst.Category.DescriptionFa,
-                              Labels = pst.Labels,
-                              PostDate = pst.PostDate,
-                              PostTime = pst.PostTime,
-                              Title = pst.Title,
-                              ViewCount = 2,
-                              ImageUrl =_image.Single(x=>x.ID==pst.ImageID).FileUrl, //pst.Image.FileUrl,
-                              IsActive = pst.IsActive
-                          }
-                            ).OrderByDescending(x => x.PostTime)
-                            .ThenByDescending(x => x.PostDate)
-                            .Skip(page * ItemsPerPage)
-                            .Take(ItemsPerPage).ToList();
-
-                //_objEntityRitualInfo.Dispose();
-                // _objEntityMediaCountInRitual.Dispose();
-                totalCount = Result.Count();
-                return Result;
-            }       
+            var Result = new List<vm_AllPost>();
+            Result = _GetAllPost()
+                     .Skip(page * ItemsPerPage)
+                     .Take(ItemsPerPage).ToList();
+            totalCount = Result.Count();
+            return Result;
         }
         [HttpPost]
         public ActionResult ContentDetail()
@@ -80,39 +46,56 @@ namespace mbensaeed.Controllers
         [HttpGet]
         public ActionResult ContentDetail(int PostID)
         {
-            using (var _Context = new ApplicationDbContext())
-            {
-                var _objEntityPost = new RepositoryPattern<Post>(_Context);
-                var _post = _objEntityPost.SearchFor(x => x.ID == PostID ).ToList();
+            var Result = new List<vm_AllPost>();
+            Result = _GetAllPost().ToList();
 
-                //var _objEntityActivity = new RepositoryPattern<Activity>(_Context);
-                //var _activity = _objEntityActivity.GetAll();
+            return View(Result);
+        }
+   
+    public IEnumerable<vm_AllPost> _GetAllPost()
+    {
+        using (var _Context = new ApplicationDbContext())
+        {
+            var _objEntityPost = new RepositoryPattern<Post>(_Context);
+            var _post = _objEntityPost.SearchFor(x => x.IsActive == "1").ToList();
 
-                var _objEntityImage = new RepositoryPattern<Image>(_Context);
-                var _image = _objEntityImage.GetAll();
+            var _objEntityActivity = new RepositoryPattern<Activity>(_Context);
+            var _activity = _objEntityActivity.GetAll();
 
-                var Result = new List<vm_AllPost>();
-                Result = (from pst in _post
-                              //join ac in _activity
-                              //on pst.ID equals ac.Posts.ID into act
-                              //from PstAc in act.DefaultIfEmpty()
-                          select new vm_AllPost
-                          {
-                              PostID = pst.ID,
-                              Content = pst.Content,
-                              LikeCount = 1,//totalCount(ac.Posts.ID),
-                              Category = "ورزش",//pst.Category.DescriptionFa,
-                              Labels = pst.Labels,
-                              PostDate = pst.PostDate,
-                              PostTime = pst.PostTime,
-                              Title = pst.Title,
-                              ViewCount = 2,
-                              ImageUrl = _image.Single(x => x.ID == pst.ImageID).FileUrl, //pst.Image.FileUrl,
-                              IsActive = pst.IsActive
-                          }
-                            ).ToList();
+            var _objEntityImage = new RepositoryPattern<Image>(_Context);
+            var _image = _objEntityImage.GetAll();
 
-                return View(Result);
+            var Result = new List<vm_AllPost>();
+            Result = (from pst in _post
+                      join im in _image
+                      on pst.ImageID equals im.ID
+                      //join ac in _activity
+                      //on pst.ID equals ac.Posts.ID into act
+                      //from PstAc in act.DefaultIfEmpty()
+                      select new vm_AllPost
+                      {
+                          PostID = pst.ID,
+                          Content = pst.Content,
+                          LikeCount = 1,//totalCount(ac.Posts.ID),
+                          Category = "ورزش",//pst.Category.DescriptionFa,
+                          Labels = pst.Labels,
+                          PostDate = pst.PostDate,
+                          PostTime = pst.PostTime,
+                          Title = pst.Title,
+                          ViewCount = 2,
+                          ImageUrl = im.FileUrl.Substring(1),//_image.Single(x=>x.ID==pst.ImageID).FileUrl, //pst.Image.FileUrl,
+                          IsActive = pst.IsActive
+                      }
+                        ).OrderByDescending(x => x.PostTime)
+                        .ThenByDescending(x => x.PostDate)
+                       .ToList();
+
+            _objEntityPost.Dispose();
+            _objEntityActivity.Dispose();
+            _objEntityImage.Dispose();
+
+            return Result;
         }
     }
+}
 }
