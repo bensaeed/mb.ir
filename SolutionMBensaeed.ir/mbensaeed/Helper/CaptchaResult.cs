c0a834dcc0fc58912f4617662f4a01bcc03fd9a7
mbensaeed <mbensaeed@bipc.org.ir>
2019-11-26 16:29:17 +0330

    a

---
+++ SolutionMBensaeed.ir/mbensaeed/Helper/CaptchaResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mbensaeed.Helper
+{
+    public class CaptchaResult
+    {
+        public string GetCaptchaString(int length)
+        {
+            return "s";
+        }
+    }
+}
