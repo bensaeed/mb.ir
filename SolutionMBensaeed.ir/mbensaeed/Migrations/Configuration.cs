7a468cd6f336962419c4b36282ebce978eb28bd7
mbensaeed <mbensaeed@bipc.org.ir>
2020-01-28 16:27:12 +0330

    Complete For First Publish

--- SolutionMBensaeed.ir/mbensaeed/Migrations/Configuration.cs
+++ SolutionMBensaeed.ir/mbensaeed/Migrations/Configuration.cs
@@ -1,4 +1,4 @@
-namespace mbensaeed.Migrations
+﻿namespace mbensaeed.Migrations
 {
     using System;
     using System.Data.Entity;
@@ -18,6 +18,25 @@ namespace mbensaeed.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
-        }
+            context.Category.AddOrUpdate(x => x.ID,
+                new Models.Category() { ID = 1, DescriptionFa = "اخبار و رسانه ها", IsActive = "1" },
+                new Models.Category() { ID = 2, DescriptionFa = "رایانه و اینترنت", IsActive = "1" },
+                new Models.Category() { ID = 3, DescriptionFa = "تجارت و اقتصاد", IsActive = "1" },
+                new Models.Category() { ID = 4, DescriptionFa = "فرهنگ و تاریخ", IsActive = "1" },
+                new Models.Category() { ID = 5, DescriptionFa = "ورزش", IsActive = "1" },
+                new Models.Category() { ID = 6, DescriptionFa = "شخصی", IsActive = "1" },
+                new Models.Category() { ID = 7, DescriptionFa = "سفر و توریسم", IsActive = "1" },
+                new Models.Category() { ID = 8, DescriptionFa = "آموزش", IsActive = "1" },
+                new Models.Category() { ID = 9, DescriptionFa = "هنر و ادبیات", IsActive = "1" },
+                new Models.Category() { ID = 10, DescriptionFa = "علم و فن آوری", IsActive = "1" },
+                new Models.Category() { ID = 11, DescriptionFa = "اندیشه و مذهب", IsActive = "1" },
+                new Models.Category() { ID = 12, DescriptionFa = "سرگرمی و طنز", IsActive = "1" },
+                new Models.Category() { ID = 13, DescriptionFa = "خانواده و زندگی", IsActive = "1" },
+                new Models.Category() { ID = 14, DescriptionFa = "سبک زندگی", IsActive = "1" }
+                );
+            context.ActivityTypes.AddOrUpdate(x => x.ID,
+                new Models.ActivityType() {ID=1,Title= "view",Description="بازديد" },
+                new Models.ActivityType() {ID=2,Title= "like",Description="پسنديدن" });
+                }
     }
 }
--- SolutionMBensaeed.ir/mbensaeed/Models/IdentityModels.cs
+++ SolutionMBensaeed.ir/mbensaeed/Models/IdentityModels.cs
@@ -30,7 +30,7 @@ namespace mbensaeed.Models
         public DbSet<Comment> Comment { get; set; }
         public DbSet<PostComment> PostComment { get; set; }
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+        : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
 
